a222ae43222476f98e68641b7cdb595becb6b300
Henrique de Azara Tosta <henriqueazara3311@gmail.com>
2023-03-28 11:19:12 -0300

    Add atividade 3

---
+++ Atividade 3 - POO2/src/entities/enums/TipoDeDocumento.java
@@ -0,0 +1,18 @@
+package entities.enums;
+
+public enum TipoDeDocumento {
+	RG("Registro Geral"),
+	CPF("Cadastro de Pessoa Física"),
+	CNPJ("Cadastro de Pessoa Juridica");
+	
+	private String descricao;
+
+	private TipoDeDocumento(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+}
