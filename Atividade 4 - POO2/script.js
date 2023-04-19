fcce9d7ad36cfb084641d2c9ecd3bab641a845b5
Henrique de Azara Tosta <henriqueazara3311@gmail.com>
2023-04-19 11:04:44 -0300

    Added Atividade 4

--- Atividade 4 - POO2/script.js
+++ Atividade 4 - POO2/script.js
@@ -8,7 +8,6 @@ const numeroTentativas = document.querySelector('#numeroTentativas')
 
 const golsBrasil = Math.floor(Math.random() * 11);
 const golsAlemanha = Math.floor(Math.random() * 11);
-console.log(golsBrasil, golsAlemanha);
 
 let tentativas = 10
 
@@ -16,31 +15,19 @@ const addPalpites = (inputBrasil, inputAlemanha) => {
     palpitesOutput.textContent += `|| ${inputBrasil} x ${inputAlemanha} `
 }
 
-btnIniciar.addEventListener('click', () => {
-    btnIniciar.style.display = 'none';
-    btnVerificar.removeAttribute('disabled');
-    btnReiniciar.style.display = '';
-});
-
-btnReiniciar.addEventListener('click', () => {
-    location.reload();
-});
-
-btnVerificar.addEventListener('click', (event) => {
-    event.preventDefault()
-    const inputBrasil = document.querySelector('#inputBrasil').value
-    const inputAlemanha = document.querySelector('#inputAlemanha').value
-    
-    --tentativas
-    resultado.textContent = ''
-    dicas.textContent = ''
-
+const resultadoPartidas = (inputBrasil, inputAlemanha) => {
     if(Number(inputBrasil) === Number(golsBrasil) 
         && Number(inputAlemanha) === Number(golsAlemanha)){
         resultado.textContent = 'Parabéns você acertou!'
+        resultado.style.backgroundColor = '#18dffe'
     }else if(tentativas === 0){
         resultado.textContent = 
             `Você não acertou o placar em 10 tentativas. O jogo terminou em Brasil ${golsBrasil} x ${golsAlemanha} Alemanha.`
+
+        resultado.style.backgroundColor = '#f77032'
+        numeroTentativas.textContent = `Número de tentativas restantes: 0`
+
+        btnVerificar.setAttribute('disabled', true)
         return
     }
         if (golsBrasil > inputBrasil) {
@@ -60,6 +47,28 @@ btnVerificar.addEventListener('click', (event) => {
     }else{
         window.alert('Insira valores válidos')
     }
+}
+
+btnIniciar.addEventListener('click', () => {
+    btnIniciar.style.display = 'none';
+    btnVerificar.removeAttribute('disabled');
+    btnReiniciar.style.display = '';
+});
+
+btnReiniciar.addEventListener('click', () => {
+    location.reload();
+});
+
+btnVerificar.addEventListener('click', (event) => {
+    event.preventDefault()
+    const inputBrasil = document.querySelector('#inputBrasil').value
+    const inputAlemanha = document.querySelector('#inputAlemanha').value
+    
+    tentativas--
+    resultado.textContent = ''
+    dicas.textContent = ''
+
+    resultadoPartidas(inputBrasil, inputAlemanha)
 
     numeroTentativas.textContent = `Número de tentativas restantes: ${tentativas}`
 })
